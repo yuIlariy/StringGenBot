c532c23ed66280e68e2807982d910ea50a660082
Ilariy <147840605+yuIlariy@users.noreply.github.com>
2026-03-23 15:24:19 +0300

    Update config.py
    Signed-off-by: Ilariy <147840605+yuIlariy@users.noreply.github.com>

--- config.py
+++ config.py
@@ -7,4 +7,4 @@ BOT_TOKEN = getenv("BOT_TOKEN")
 MONGO_URL = getenv("MONGO_URL")
 
 OWNER_ID = int(getenv("OWNER_ID", 6318135266))
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/FallenAssociation")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/OthersBees")
