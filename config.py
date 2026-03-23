9391976e2379568d2ebee26ebac58eec03cc4a29
Ilariy <147840605+yuIlariy@users.noreply.github.com>
2026-03-23 14:33:05 +0300

    Update OWNER_ID in config.py
    Signed-off-by: Ilariy <147840605+yuIlariy@users.noreply.github.com>

--- config.py
+++ config.py
@@ -6,5 +6,5 @@ load_dotenv()
 BOT_TOKEN = getenv("BOT_TOKEN")
 MONGO_URL = getenv("MONGO_URL")
 
-OWNER_ID = int(getenv("OWNER_ID", 1356469075))
+OWNER_ID = int(getenv("OWNER_ID", 6318135266))
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/FallenAssociation")
