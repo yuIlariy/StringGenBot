b36d94c3d3b05ea5d6391a719a1b551bf83e2a2b
Ilariy <147840605+yuIlariy@users.noreply.github.com>
2026-03-23 18:05:19 +0300

    Update gen.py
    Signed-off-by: Ilariy <147840605+yuIlariy@users.noreply.github.com>

--- anony/modules/gen.py
+++ anony/modules/gen.py
@@ -95,7 +95,7 @@ async def _gen_session(_, cq: types.CallbackQuery):
          return await cq.message.reply_text(f"Error : <code>{str(ex)}</code>")
 
     try:
-        txt = "Here is your {0} session\n\n<code>{1}</code>\n\nA session generator bot By <a href={2}>OtherBees</a>\n\n☠ <b>Note :</b> Don't share the session with anyone."
+        txt = "🗿Here is your {0} session\n\n<code>{1}</code>\n\n⚡A session generator bot By <a href={2}>OtherBees🐝</a>\n\n☠ <b>Note :</b> Don't share the session with anyone."
         if pyrogram:
             string_session = await client.export_session_string()
             await client.send_message(
@@ -123,6 +123,6 @@ async def _gen_session(_, cq: types.CallbackQuery):
         pass
     try:
         await client.disconnect()
-        await cq.message.reply_text(f"Successfully generated your {sgen} string session.\n\nPlease check your saved messages for getting it.\n\nA string generator bot by <a href={SUPPORT_CHAT}>OthersBees</a>.", reply_markup=buttons.pm_key(cq.from_user.id))
+        await cq.message.reply_text(f"⚡Successfully generated your {sgen} string session.\n\n🗿Please check your saved messages for getting it.\n\n🎲A string generator bot By <a href={SUPPORT_CHAT}>OtherBees🐝</a>.", reply_markup=buttons.pm_key(cq.from_user.id))
     except:
         pass
