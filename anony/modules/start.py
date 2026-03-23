42811a1d525ec9807255e1aa08ce1490bffd9ad0
Ilariy <147840605+yuIlariy@users.noreply.github.com>
2026-03-23 14:42:39 +0300

    Update start command to reply with photo and caption
    Signed-off-by: Ilariy <147840605+yuIlariy@users.noreply.github.com>

--- anony/modules/start.py
+++ anony/modules/start.py
@@ -5,8 +5,9 @@ from anony import app, buttons, db
 
 @app.on_message(filters.command(["start"]) & filters.private)
 async def f_start(_, m: types.Message):
-    await m.reply_text(
-        text=f"Hey {m.from_user.first_name},\n\nThis is {app.mention},\nAn open source session generator bot.",
+    await m.reply_photo(
+        photo="https://telegra.ph/file/d84e1a26bf8596bbad10a.jpg",
+        caption=f"Hey {m.from_user.first_name} 👋,\n\nThis is {app.mention} 🤖,\nAn open source session generator bot ⚡.\n\n🛸 powered by [NAm](https://t.me/xspes)",
         reply_markup=buttons.start_key(),
     )
     await db.add_user(m.from_user.id)
